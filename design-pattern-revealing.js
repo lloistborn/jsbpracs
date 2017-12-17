ed470951cb9dd319d9b693ff49d85fa9a6f19f3a
lloisborn <faisalmorensya@gmail.com>
2017-12-17 20:13:47 +0700

    revealing design pattern

---
+++ design-pattern-revealing.js
@@ -0,0 +1,30 @@
+// The purpose is 
+// to maintain encapsulation and reveal certain variables 
+// and methods returned in an object literal
+
+let Exposer = (function() {
+  let privateVariable = 10
+
+  let privateMethod = function() {
+    console.log("inside private method")
+    privateVariable++
+  }
+
+  let methodToExpose = function() {
+    console.log("method to expose")
+  }
+
+  let otherMethodToExpose = function() {
+    privateMethod()
+  }
+
+  return {
+    first: methodToExpose,
+    second: otherMethodToExpose
+  }
+
+})()
+
+Exposer.first()
+Exposer.second()
+Exposer.methodToExpose()
